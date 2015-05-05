4e4b20b28eac46d1703c26caeb5ae42efb4a0cd0
BlakeTheCake <blakehord@gmail.com>
2015-05-04 22:08:46 -0400

    Started Card Class
    String value, number value, constructor, started readCard()

--- WarScript.cpp
+++ WarScript.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
 
+Class Card
+{
+private:
+  string m_suit;
+  int m_num;
+  
+public:
+  Card(string suit, int num)
+  {
+    strcpy(m_suit, suit);
+    m_num = num;
+  };
+  
+  void readCard()
+  {
+    std::cout << ""
+  }
+}
+
 int main ()
 {
 
