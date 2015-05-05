d2bcd962b9b4496e0037775829c5af73766efd01
BlakeTheCake <blakehord@gmail.com>
2015-05-05 16:34:52 -0400

    Made number reader of switch statement

--- WarScript.cpp
+++ WarScript.cpp
@@ -2,6 +2,7 @@
 
 Class Card
 {
+  using namespace std;
 private:
   string m_suit;
   int m_num;
@@ -15,7 +16,35 @@ public:
   
   void readCard()
   {
-    std::cout << ""
+    switch (m_num)
+    {
+      case 1:
+        cout << "Ace";
+        break;
+      case 2:
+      case 3:
+      case 4:
+      case 5:
+      case 6:
+      case 7:
+      case 8:
+      case 9:
+      case 10:
+        cout << m_num;
+        break;
+      case 11:
+        cout << "Jack";
+        break;
+      case 12:
+        cout << "Queen";
+        break;
+      case 13:
+        cout << "King";
+        break;
+      default:
+        cout << "Corrupted Card! RESTART";
+        break;
+    };
   }
 }
 
